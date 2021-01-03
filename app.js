14f84925bce6b3b31f7865851853913cf513c91a
panda2k <thelowkeypanda@gmail.com>
2021-01-02 19:53:20 -0800

    update bot to not spam chat - checks if activity name is still the same as before also make alert channel id an environment variable

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js')
 
 const client = new Discord.Client()
-const alertChannelId = '735948172488081658'
+const alertChannelId = process.env.OUTPUT_CHANNEL_ID
 const imgLink = 'https://i.imgur.com/xGtV8Lm.png'
 
 client.on('ready', () => {
@@ -10,10 +10,15 @@ client.on('ready', () => {
 
 client.on('presenceUpdate', (oldMember, newMember) => {
     console.log(newMember.member.displayName)
-
     for (i = 0; i < newMember.activities.length; i++) {
         let currentApp = newMember.activities[i].name
         if (currentApp == 'League of Legends' || currentApp == 'VALORANT') {
+            for (j = 0; j < oldMember.activities.length; j++) {
+                if (oldMember.activities[j].name == 'League of Legends' || oldMember.activities[j].name == 'VALORANT') {
+                    return // activity name didn't change but other metadata did
+                }
+            }
+
             client.channels.fetch(alertChannelId)
             .then(async channel => {
                 await channel.send('', {files: [imgLink]})
