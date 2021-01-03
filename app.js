1fd7bef157256dc40cf9a8964251415b9e6d3714
panda2k <thelowkeypanda@gmail.com>
2021-01-03 13:34:38 -0800

    prevent bot from double pinging when status temporarily dissapears

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const client = new Discord.Client()
 const alertChannelId = process.env.OUTPUT_CHANNEL_ID
 const imgLink = 'https://i.imgur.com/xGtV8Lm.png'
 
+var userActivityHistory = {}
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`)
 })
 
 client.on('presenceUpdate', (oldMember, newMember) => {
     console.log(newMember.member.displayName)
+    let currentTime = new Date().getTime() / 1000 // time stamp in seconds
+
     for (i = 0; i < newMember.activities.length; i++) {
         let currentApp = newMember.activities[i].name
         if (currentApp == 'League of Legends' || currentApp == 'VALORANT') {
@@ -18,6 +22,19 @@ client.on('presenceUpdate', (oldMember, newMember) => {
                     return // activity name didn't change but other metadata did
                 }
             }
+            try {
+                if (currentTime - userActivityHistory[newMember.member.id].timeStamp < 120) { // check if status just temporarily dissapeared 
+                    if (currentApp == userActivityHistory[newMember.member.id].activity) {
+                        return
+                    }
+                } 
+            } catch (error) {
+                if (!(error instanceof TypeError)) {
+                    console.log(error)
+                }
+            }
+
+            userActivityHistory[newMember.member.id] = { 'timeStamp': currentTime, 'activity': currentApp }
 
             client.channels.fetch(alertChannelId)
             .then(async channel => {
@@ -27,6 +44,7 @@ client.on('presenceUpdate', (oldMember, newMember) => {
         }
     }
     console.log(newMember.activities)
+    console.log(userActivityHistory)
 })
 
 client.login(process.env.GAMER_BOT_SECRET)
