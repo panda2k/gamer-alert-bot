201cb9480760a73160b12dfff45d80d18fa4c0a4
panda2k <thelowkeypanda@gmail.com>
2021-01-16 20:34:50 -0800

    add ability to get session count in a date range

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const GamerAlert = require('./gameralert')
 
 const client = new Discord.Client()
 
+let formatDate = date => {
+    return [date.getFullYear(), date.getMonth() + 1, date.getDate()].join('-')
+}
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`)
 })
@@ -49,13 +53,50 @@ client.on('message', async message => {
             })
             .catch(error => {
                 if (error.response) {
-                    console.log(error.response.data.error)
                     message.channel.send(error.response.data.error)
                 } else {
                     message.channel.send(error)
                 }
             })
-    } 
+    } else if (command == 'sessions') {
+        // format: ?sessions <discordMention> <timePeriod>
+        if (args.length < 2) {
+            message.channel.send('Missing arguments. Follow this command format: `?sessions @personhere <timePeriod>`.' + 
+                                'Time period can be either `today`, `week`, `month` or a custom date range like this: `2020-01-15-2020-01-26`')
+            return 
+        }
+
+        let userId = args[0].slice(3, args[0].length - 1)
+        let startDate
+        let endDate = new Date()
+
+        if (args[1] == 'today') {
+            startDate = new Date(endDate.getFullYear(), endDate.getMonth(), endDate.getDate())
+        } else if (args[1] == 'week') {
+            startDate = new Date(endDate.getTime() - (604800 * 1000))
+        } else if (args[1] == 'month') {
+            startDate = new Date(endDate.getTime() - (43800 * 60 * 1000))
+        } else if (args[1].split('-').length == 6) {
+            let splitDate = args[1].split('-')
+            startDate = new Date(splitDate[0], splitDate[1] - 1, splitDate[2])
+            endDate = new Date(splitDate[3], splitDate[4] - 1, splitDate[5])
+        } else {
+            message.channel.send('Invalid time period. Time period can be either `today`, `week`, `month` or a custom date range like this: `2020-01-15-2020-01-26`')
+            return
+        }
+
+        await GamerAlert.getUserSessions(userId, startDate.getTime(), endDate.getTime())
+            .then(result => {
+                message.channel.send(`<@${userId}> has logged ${result.length} sessions between ${formatDate(startDate)} and ${formatDate(endDate)}`)
+            })
+            .catch(error => {
+                if (error.response) {
+                    message.channel.send(error.response.data.error)
+                } else {
+                    message.channel.send(error)
+                }
+            })
+    }
     else {
         message.channel.send('Unknown command')
     }
--- gameralert.js
+++ gameralert.js
@@ -6,6 +6,10 @@ const requestClient = axios.create({
     baseURL: API_URL
 })
 
+const getAllServers = async () => (await requestClient.get(`/servers`)).data
+
+const getAllPopulatedServers = async () => (await requestClient.get('/servers/full')).data
+
 const getServer = async (serverId) => (await requestClient.get(`/servers/${serverId}`)).data
 
 const addServer = async (serverId) => (await requestClient.post('/servers', { id: serverId })).data
@@ -14,7 +18,23 @@ const registerUser = async(discordId, leagueName) => (await requestClient.post('
 
 const addUserToServer = async(serverId, discordId) => (await requestClient.post(`/servers/${serverId}/users`, { discordId: discordId })).data
 
+const updateServerWebhook = async(serverId, webhookUrl) => (await requestClient.patch(`/servers/${serverId}`, { webhook: webhookUrl })).data
+
+const createSession = async(discordId) => (await requestClient.post(`/users/${discordId}/sessions`)).data
+
+const addGame = async(sessionId, gameId) => (await requestClient.post(`/sessions/${sessionId}/game`, { gameId: gameId })).data
+
+const getUserSessions = async (discordId, startDate, endDate) => {
+    return (await requestClient.get(`/users/${discordId}/sessions`, { data: { startDate: startDate, endDate: endDate } })).data
+}
+
 module.exports.getServer = getServer
+module.exports.getAllPopulatedServers = getAllPopulatedServers
 module.exports.addServer = addServer
 module.exports.registerUser = registerUser
 module.exports.addUserToServer = addUserToServer
+module.exports.updateServerWebhook = updateServerWebhook
+module.exports.getAllServers = getAllServers
+module.exports.createSession = createSession
+module.exports.addGame = addGame
+module.exports.getUserSessions = getUserSessions
